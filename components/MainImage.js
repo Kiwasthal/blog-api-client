88b76d1e974c110df5e95d562c10ef841ef74cf5
Steerpike <imthekoul@gmail.com>
2022-09-08 03:57:56 +0300

    integration

--- components/MainImage.js
+++ components/MainImage.js
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import MainImage from '../public/blog-main-image.jpg';
 import MainHeader from './MainHeader';
+import { AnimatedTextUpper, AnimatedTextLower } from './MainSubHeader';
 
 const IndexImageSlot = () => {
   return (
     <div className="w-screen h-screen relative border-black border-b-8 border-solid">
       <MainHeader />
+      <AnimatedTextUpper />
+      <AnimatedTextLower />
       <Image
         src={MainImage}
         layout="fill"
