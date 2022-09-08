85d4e32b5623c4df97ebdba558bba36840188c6b
Steerpike <imthekoul@gmail.com>
2022-09-08 23:06:06 +0300

    Login post request added with LS update

--- components/Login.js
+++ components/Login.js
@@ -10,10 +10,6 @@ const Login = ({ updateUserAuth }) => {
     password: '',
   };
 
-  const onSubmit = values => {
-    console.log(values);
-  };
-
   const validationSchema = Yup.object({
     username: Yup.string()
       .required('This field is required!')
@@ -22,32 +18,6 @@ const Login = ({ updateUserAuth }) => {
       .required('This field is required!')
       .min(6, 'Password must be at least 6 characters long!'),
   });
-  // onSubmit={async (values, { setSubmitting }) => {
-  //   const formData = JSON.stringify(values);
-  //   try {
-  //     const request = await fetch('http://localhost:3000/api/login', {
-  //       method: 'POST',
-  //       body: formData,
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     });
-  //     const resJson = await request.json();
-  //     if (request.stats === 200) {
-  //       await updateUserAuth(true);
-  //       localStorage.setItem('token', resJson.token);
-  //       localStorage.setItem('userAuth', true);
-  //       localStorage.setItem('username', myJson.body.username);
-  //       localStorage.setItem('id', myJson.body._id);
-  //       router.back();
-  //     } else {
-  //       setErrMsg(resJson.info.message);
-  //       setLogErr(true);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }}
 
   return (
     <div className="w-full mt-5  flex  flex-col items-center">
@@ -65,80 +35,117 @@ const Login = ({ updateUserAuth }) => {
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={onSubmit}
+        onSubmit={async (values, { setFieldError }) => {
+          const formData = JSON.stringify(values);
+
+          try {
+            const request = await fetch('http://localhost:3000/api/login', {
+              method: 'POST',
+              body: formData,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+            const resJson = await request.json();
+            if (request.status === 200) {
+              await updateUserAuth(true);
+              localStorage.setItem('token', resJson.token);
+              localStorage.setItem('userAuth', true);
+              localStorage.setItem('username', resJson.body.username);
+              localStorage.setItem('id', resJson.body._id);
+              router.back();
+            } else {
+              resJson.info.message === 'Incorrect Password'
+                ? setFieldError('password', resJson.info.message)
+                : setFieldError('username', resJson.info.message);
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        }}
         validateOnChange={false}
       >
-        <Form className="bg-white shadow-md rounded w-1/2 lg:w-2/5 pt-6 pb-8 mb-4 p-8">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-xl font-bold mb-2"
-              htmlFor="username"
-            >
-              Username
-            </label>
-            <Field name="username">
-              {({ field, meta }) => (
-                <input
-                  id="username"
-                  type="username"
-                  placeholder="User Name"
-                  {...field}
-                  className={
-                    meta.error && meta.touched
-                      ? 'border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                      : 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  }
-                />
-              )}
-            </Field>
-            <ErrorMessage name="username">
-              {errorMsg => (
-                <p className="text-red-500 text-xs italic mt-1" name="password">
-                  {errorMsg}
-                </p>
-              )}
-            </ErrorMessage>
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-xl font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <Field name="password">
-              {({ field, meta }) => (
-                <input
-                  id="password"
-                  type="password"
-                  placeholder="******************"
-                  {...field}
-                  className={
-                    meta.error && meta.touched
-                      ? 'border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                      : 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  }
-                />
-              )}
-            </Field>
-            <ErrorMessage name="password">
-              {errorMsg => (
-                <p className="text-red-500 text-xs italic mt-1" name="password">
-                  {errorMsg}
-                </p>
-              )}
-            </ErrorMessage>
-          </div>
+        {formik => {
+          return (
+            <Form className="bg-white shadow-md rounded w-1/2 lg:w-2/5 pt-6 pb-8 mb-4 p-8">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-xl font-bold mb-2"
+                  htmlFor="username"
+                >
+                  Username
+                </label>
+                <Field name="username">
+                  {({ field, meta }) => (
+                    <input
+                      id="username"
+                      type="username"
+                      placeholder="User Name"
+                      {...field}
+                      className={
+                        meta.error && meta.touched
+                          ? 'border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                          : 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      }
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="username">
+                  {errorMsg => (
+                    <p
+                      className="text-red-500 text-xs italic mt-1"
+                      name="password"
+                    >
+                      {errorMsg}
+                    </p>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-xl font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <Field name="password">
+                  {({ field, meta }) => (
+                    <input
+                      id="password"
+                      type="password"
+                      placeholder="******************"
+                      {...field}
+                      className={
+                        meta.error && meta.touched
+                          ? 'border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                          : 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      }
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="password">
+                  {errorMsg => (
+                    <p
+                      className="text-red-500 text-xs italic mt-1"
+                      name="password"
+                    >
+                      {errorMsg}
+                    </p>
+                  )}
+                </ErrorMessage>
+              </div>
 
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Log In
-            </button>
-          </div>
-        </Form>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Log In
+                </button>
+              </div>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
