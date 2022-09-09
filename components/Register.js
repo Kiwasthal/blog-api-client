35d3e49bec738b0d8a48ca1287182e2e9fe2352d
Steerpike <imthekoul@gmail.com>
2022-09-09 04:01:29 +0300

    register page configuration

--- components/Register.js
+++ components/Register.js
@@ -1,39 +1,28 @@
 import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
-import { Formik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-const Register = ({ updateUserAuth }) => {
+const Register = () => {
   const router = useRouter();
-  // const [logErr, setLogErr] = useState(false);
-  // const [errMsg, setErrMsg] = useState('');
 
-  // const submitForm = async (data, e) => {
-  //   const formData = JSON.stringify(data);
+  const initialValues = {
+    username: '',
+    password: '',
+    confirmPassword: '',
+  };
 
-  //   try {
-  //     const request = await fetch('http://localhost:3000/api/login', {
-  //       method: 'POST',
-  //       body: formData,
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     });
-  //     const resJson = await request.json();
-  //     if (request.stats === 200) {
-  //       await updateUserAuth(true);
-  //       localStorage.setItem('token', resJson.token);
-  //       localStorage.setItem('userAuth', true);
-  //       localStorage.setItem('username', myJson.body.username);
-  //       localStorage.setItem('id', myJson.body._id);
-  //       router.back();
-  //     } else {
-  //       setErrMsg(resJson.info.message);
-  //       setLogErr(true);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .required('This field is required!')
+      .min(3, 'Username must be at least 3 characters long'),
+    password: Yup.string()
+      .required('This field is required!')
+      .min(6, 'Password must be at least 6 characters long!'),
+    confirmPassword: Yup.string()
+      .required('This field is required!')
+      .oneOf([Yup.ref('password')], 'Passwords do not match!'),
+  });
 
   return (
     <div className="w-full mt-5  flex  flex-col items-center">
@@ -45,21 +34,16 @@ const Register = ({ updateUserAuth }) => {
           className="text-5xl font-bold text-gray-900 px-2  "
           initial="hidden"
         >
-          Register
+          Sign-Up
         </motion.h1>
       </motion.div>
       <Formik
-        initialValues={{ username: '', password: '', confirmpass: '' }}
-        validate={values => {
-          const errors = {};
-          if (!values.username) errors.username = 'Required';
-          else if (values.password !== values.confirmpass)
-            errors.password = 'Passwords do not match';
-
-          return errors;
-        }}
-        onSubmit={async (values, { setSubmitting }) => {
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        validateOnChange={false}
+        onSubmit={async (values, { setFieldError, errors }) => {
           const formData = JSON.stringify(values);
+
           try {
             const request = await fetch('http://localhost:3000/api/register', {
               method: 'POST',
@@ -69,32 +53,17 @@ const Register = ({ updateUserAuth }) => {
               },
             });
             const resJson = await request.json();
-            if (request.stats === 200) {
-              await updateUserAuth(true);
-              localStorage.setItem('token', resJson.token);
-              localStorage.setItem('userAuth', true);
-              localStorage.setItem('username', myJson.body.username);
-              localStorage.setItem('id', myJson.body._id);
-              router.back();
-            } else {
-              setErrMsg(resJson.info.message);
-              setLogErr(true);
+            if (request.status === 200) router.replace('/login');
+            else {
+              resJson.errors[0].msg === 'Error: Username already exists'
+                ? setFieldError('username', 'Username already exists')
+                : console.log('unhandler error');
             }
-          } catch (err) {
-            console.log(err);
-          }
+          } catch (err) {}
         }}
       >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <form className="bg-white shadow-md rounded w-1/2 lg:w-2/5 pt-6 pb-8 mb-4 p-8">
+        {({ isSubmitting }) => (
+          <Form className="bg-white shadow-md rounded w-1/2 lg:w-2/5 pt-6 pb-8 mb-4 p-8">
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-xl font-bold mb-2"
@@ -102,14 +71,31 @@ const Register = ({ updateUserAuth }) => {
               >
                 Username
               </label>
-              <input
-                className="shadow-md appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none  focus:outline-slate-400"
-                id="username"
-                name="username"
-                type="text"
-                placeholder="Username"
-              />
-              <p className="text-red-500 text-xs italic"></p>
+              <Field name="username">
+                {({ field, meta }) => (
+                  <input
+                    id="username"
+                    type="text"
+                    placeholder="User Name"
+                    {...field}
+                    className={
+                      meta.error && meta.touched
+                        ? 'border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                        : 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:outline-slate-400'
+                    }
+                  />
+                )}
+              </Field>
+              <ErrorMessage name="username">
+                {errorMsg => (
+                  <p
+                    className="text-red-500 text-xs italic mt-1"
+                    name="password"
+                  >
+                    {errorMsg}
+                  </p>
+                )}
+              </ErrorMessage>
             </div>
             <div className="mb-4">
               <label
@@ -118,14 +104,31 @@ const Register = ({ updateUserAuth }) => {
               >
                 Password
               </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:outline-slate-400 "
-                id="password"
-                name="password"
-                type="password"
-                placeholder="******************"
-              />
-              <p className="text-red-500 text-xs italic"></p>
+              <Field name="password">
+                {({ field, meta }) => (
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="******************"
+                    {...field}
+                    className={
+                      meta.error && meta.touched
+                        ? 'border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                        : 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:outline-slate-400'
+                    }
+                  />
+                )}
+              </Field>
+              <ErrorMessage name="password">
+                {errorMsg => (
+                  <p
+                    className="text-red-500 text-xs italic mt-1"
+                    name="password"
+                  >
+                    {errorMsg}
+                  </p>
+                )}
+              </ErrorMessage>
             </div>
             <div className="mb-6">
               <label
@@ -134,24 +137,68 @@ const Register = ({ updateUserAuth }) => {
               >
                 Confirm Password
               </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:outline-slate-400"
-                id="confirmPassword"
-                name="confirmPassword"
-                type="confirmPassword"
-                placeholder="******************"
-              />
-              <p className="text-red-500 text-xs italic"></p>
+              <Field name="confirmPassword">
+                {({ field, meta }) => (
+                  <input
+                    id="confirmPassword"
+                    type="password"
+                    placeholder="******************"
+                    {...field}
+                    className={
+                      meta.error && meta.touched
+                        ? 'border-red-500 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                        : 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:outline-slate-400'
+                    }
+                  />
+                )}
+              </Field>
+              <ErrorMessage name="confirmPassword">
+                {errorMsg => (
+                  <p
+                    className="text-red-500 text-xs italic mt-1"
+                    name="confirmPassword"
+                  >
+                    {errorMsg}
+                  </p>
+                )}
+              </ErrorMessage>
             </div>
             <div className="flex items-center justify-between">
-              <button
-                className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-              >
-                Register
-              </button>
+              {isSubmitting ? (
+                <button
+                  disabled
+                  type="button"
+                  className=" bg-gray-900 hover:bg-gray-700  focus:outline-none  font-bold first-line:rounded-lg text-white px-5 py-2 text-sm text-center mr-2 rounded inline-flex items-center"
+                >
+                  <svg
+                    aria-hidden="true"
+                    role="status"
+                    className="inline mr-3 w-4 h-4 text-white animate-spin"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="#E5E7EB"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  Loading...
+                </button>
+              ) : (
+                <button
+                  className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Register
+                </button>
+              )}
             </div>
-          </form>
+          </Form>
         )}
       </Formik>
     </div>
