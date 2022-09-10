ef17caaa23b1fb6508ba76147f1e1e951cab9d87
Steerpike <imthekoul@gmail.com>
2022-09-10 19:05:03 +0300

    comment intro animate

--- components/Comment.js
+++ components/Comment.js
@@ -1,8 +1,39 @@
 import moment from 'moment';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 
 const Comment = ({ comment, index }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  const commentVariants = {
+    hidden: index => ({
+      x: index % 2 === 1 ? 100 : -100,
+      opacity: 0,
+    }),
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1,
+        delay: 0.5,
+      },
+    },
+  };
+
+  useEffect(() => {
+    if (inView) controls.start('visible');
+  }, [controls, inView]);
+
   return (
-    <div className="p-2  border-gray-700  border-l-4 w-2/3 md:w-4/5 mb-2 shadow-md mt-4">
+    <motion.div
+      className="p-2  border-gray-700  border-l-4 w-2/3 md:w-4/5 mb-2 shadow-md mt-4"
+      variants={commentVariants}
+      animate={controls}
+      initial="hidden"
+      custom={index}
+    >
       <div className="flex justify-between px-2 pt-1">
         <span className="font-bold text-gray-900 text-lg">
           {comment.user.username} :
@@ -27,8 +58,9 @@ const Comment = ({ comment, index }) => {
         >
           <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"></path>
         </svg>
+        <span ref={ref}></span>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
