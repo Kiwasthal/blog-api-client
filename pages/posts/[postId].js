bfd289a5da379aea18727466b77f782143ea0fe1
Steerpike <imthekoul@gmail.com>
2022-09-10 19:20:21 +0300

    comment page stylong and likes text updates

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -56,12 +56,12 @@ const Post = ({ post, comments, userAuth }) => {
         setRegenerating(false);
       }
     })();
-  }, [currentComments, regenerating]);
+  }, [currentComments, regenerating, query.postId]);
 
   return (
     <div className="page">
       <div className="flex flex-col items-center ">
-        <div className="shadow-lg pb-12 px-4 mt-8 flex flex-col  items-center justify-center lg:w-3/4 md:w-4/5 border-l-2 border-r-2 border-gray-900">
+        <div className="shadow-lg pb-6 px-4 mt-8 flex flex-col  items-center justify-center lg:w-3/4 md:w-4/5 border-l-4 border-r-4 border-gray-700">
           <div className="flex flex-col items-center mb-6">
             <motion.div
               className="text-6xl font-extrabold text-gray-900"
@@ -85,14 +85,30 @@ const Post = ({ post, comments, userAuth }) => {
           </div>
 
           <p>{post.content}</p>
-          <p className="text-lg text-gray-500 font-bold mt-8">
-            Author{' '}
-            <span className="text-gray-900 font-extrabold">
-              <em>: {post.user.username}</em>
-            </span>
-          </p>
+          <hr />
+          <div className="flex justify-between items-center  mt-8 gap-2 w-2/3 md:w-4/5 mb-2">
+            <p className="text-lg text-gray-500 font-bold">
+              Author{' '}
+              <span className="text-gray-900 font-extrabold">
+                <em>: {post.user.username}</em>
+              </span>
+            </p>
+            <div className="flex gap-2 ">
+              <span className="text-gray-900  font-extrabold text-lg">
+                Likes : {post.likes.length}
+              </span>
+              <svg
+                className="w-6 h-6 hover:shadow-2xl cursor-pointer hover:scale-150 transition-transform ease-out"
+                fill="#111827"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"></path>
+              </svg>
+            </div>
+          </div>
         </div>
-        <div className="shadow-lg pb-12 px-4 mt-8 flex flex-col   justify-center lg:w-3/4 md:w-4/5 border-l-2 border-r-2 border-gray-900">
+        <div className="shadow-lg pb-12 px-4 mt-8 flex flex-col   justify-center lg:w-3/4 md:w-4/5 border-l-4 border-r-4 border-gray-700">
           <section className="mt-4 flex flex-col items-center mb-6 ">
             <div className="flex items-center my-4 before:flex-1 before:border-2 before:border-gray-900 before:mt-0.5 after:flex-1 after:border-2 after:border-gray-900 after:mt-0.5 w-2/3 md:w-4/5 mb-6 ">
               <h1 className="text-5xl font-bold text-gray-900 px-2  ">
